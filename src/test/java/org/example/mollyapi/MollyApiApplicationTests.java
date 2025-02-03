f80be7fec2cc17b01b992eedede9fe5773d7b0f6
LeeJeYoun <dlwpdbs1229@naver.com>
2025-02-03 14:57:16 +0900

    init commit

---
+++ src/test/java/org/example/mollyapi/MollyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mollyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
