f7c3f9501bd90150d562b4c265bb611c6f1710bd
Lee Ye Jin <jene0028@gmail.com>
2025-02-12 10:07:24 +0900

    feat: Order 엔티티 생성

---
+++ src/main/java/org/example/mollyapi/order/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package org.example.mollyapi.order.repository;
+
+import org.example.mollyapi.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findById(Long orderId);
+}
