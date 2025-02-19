062c3d171978b28746c77bb157483501992f9663
LeeJeYoun <dlwpdbs1229@naver.com>
2025-02-19 13:25:23 +0900

    [#23]feat: 사용자 검색어 기반 상품 조회 추가

--- src/main/java/org/example/mollyapi/search/controller/SearchController.java
+++ src/main/java/org/example/mollyapi/search/controller/SearchController.java
@@ -1,9 +1,17 @@
 package org.example.mollyapi.search.controller;
 
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.mollyapi.search.dto.SearchItemResDto;
 import org.example.mollyapi.search.service.SearchService;
+import org.example.mollyapi.user.dto.GetUserInfoResDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(name = "Search Controller", description = "검색 관련 엔드 포인트")
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
@@ -23,7 +32,12 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping
-    public ResponseEntity<?> search(@RequestParam(required = false) String keyword,
+    @Operation(summary = "검색어 기반 상품 조회 기능", description = "검색어 기반 상품 검색")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공",
+                    content = @Content(schema = @Schema(implementation = SearchItemResDto.class))),
+    })
+    public ResponseEntity<List<SearchItemResDto>> search(@RequestParam(required = false) String keyword,
                                     @RequestParam(required = false) Long cursorId,
                                     @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime lastCreatedAt,
                                     Pageable pageable) {
