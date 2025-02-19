00ba4b19ee5aa97579cd59c371362608304e1a3f
Jieun714 <jieun98.dev@gmail.com>
2025-02-19 17:06:23 +0900

    [#81]fix: Dto 명칭 수정

--- src/main/java/org/example/mollyapi/review/dto/response/GetReviewResDto.java
+++ src/main/java/org/example/mollyapi/review/dto/response/GetReviewResDto.java
@@ -3,7 +3,7 @@ package org.example.mollyapi.review.dto.response;
 import java.util.List;
 
 public record GetReviewResDto(
-        ReviewInfo reviewInfo,
+        ReviewInfoDto reviewInfo,
         List<String> images //리뷰 이미지
 ) {
 }
