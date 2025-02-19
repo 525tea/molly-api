20a52fbd56c2d3c8e5390bb86cfa56eb7d3fb366
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2025-02-19 11:22:53 +0900

    [#71] feat: 조회수 순서로 정렬 기능 추가

--- src/main/java/org/example/mollyapi/product/dto/ProductAndThumbnailDto.java
+++ src/main/java/org/example/mollyapi/product/dto/ProductAndThumbnailDto.java
@@ -2,10 +2,8 @@ package org.example.mollyapi.product.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
-import org.example.mollyapi.product.entity.ProductImage;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class ProductAndThumbnailDto {
@@ -18,6 +16,8 @@ public class ProductAndThumbnailDto {
     String url;
     String filename;
     LocalDateTime createdAt;
+    Long viewCount;
+    Long purchaseCount;
 
 
     @QueryProjection
@@ -30,7 +30,9 @@ public class ProductAndThumbnailDto {
             String description,
             String url,
             String filename,
-            LocalDateTime createdAt
+            LocalDateTime createdAt,
+            Long viewCount,
+            Long purchaseCount
     ) {
         this.id = id;
         this.categoryId = categoryId;
@@ -41,5 +43,7 @@ public class ProductAndThumbnailDto {
         this.url = url;
         this.filename = filename;
         this.createdAt = createdAt;
+        this.viewCount = viewCount;
+        this.purchaseCount = purchaseCount;
     }
 }
