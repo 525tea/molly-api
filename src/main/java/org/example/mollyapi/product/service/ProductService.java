abf5decce442131ffd00afb0341c469f125a44d5
LeeJeYoun <dlwpdbs1229@naver.com>
2025-03-06 12:48:15 +0900

    [#165] test: JWT 테스트 코드 작성 Password 테스트 코드 작성 SignIn 테스트 코드 작성

---
+++ src/main/java/org/example/mollyapi/product/service/ProductService.java
@@ -0,0 +1,31 @@
+package org.example.mollyapi.product.service;
+import org.example.mollyapi.product.dto.BrandSummaryDto;
+import org.example.mollyapi.product.dto.ProductDto;
+import org.example.mollyapi.product.dto.ProductFilterCondition;
+import org.example.mollyapi.product.dto.ProductItemDto;
+import org.example.mollyapi.product.dto.response.ColorDetailDto;
+import org.example.mollyapi.product.dto.response.ProductResDto;
+import org.example.mollyapi.product.entity.Product;
+import org.example.mollyapi.product.entity.ProductItem;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    ProductResDto registerProduct(
+            Long userId,
+            ProductDto productDto,
+            List<ProductItemDto> productItemDtoList,
+            MultipartFile thumbnailImage,
+            List<MultipartFile> productImages,
+            List<MultipartFile> descriptionImages
+            );
+    ProductResDto updateProduct(Long userId, Long id, ProductDto productDto, List<ProductItemDto> productItemDtoList);
+    void deleteProduct(Long userId, Long id);
+
+    List<ColorDetailDto> groupItemByColor(List<ProductItem> items);
+    ProductResDto convertToProductResDto(Product product);
+}
