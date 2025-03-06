1a6c9c7bd2011a190f751c557dab915b5f1af098
LeeJeYoun <dlwpdbs1229@naver.com>
2025-03-06 13:46:34 +0900

    [#165] fix: ProductReadServiceImpl에 빈주입

--- src/main/java/org/example/mollyapi/product/service/ProductReadService.java
+++ src/main/java/org/example/mollyapi/product/service/ProductReadService.java
@@ -5,11 +5,10 @@ import org.example.mollyapi.product.dto.ProductFilterCondition;
 import org.example.mollyapi.product.dto.response.ProductResDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+
 public interface ProductReadService {
     Slice<ProductResDto> getAllProducts(ProductFilterCondition condition, Pageable pageable);
     Slice<BrandSummaryDto> getPopularBrand(Pageable pageable);
